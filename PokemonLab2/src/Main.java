bd905c42ebb128b83cddd05bf5282c72e1275c53
Maxim Pomogaev <coldfrontlich@gmail.com>
2022-10-11 23:51:07 +0300

    make jar

--- PokemonLab2/src/Main.java
+++ PokemonLab2/src/Main.java
@@ -4,10 +4,10 @@ import ru.ifmo.se.pokemon.*;
 public class Main {
     public static void main(String[] args) {
         Battle b = new Battle();
-        b.addAlly(new Honchkrow("Bibich", 2));
-        b.addAlly(new Murkrow("Bubech", 1));
-        b.addFoe(new Tsareena("Jesus", 3));
-        b.addFoe(new Murkrow("Jake", 3));
+        b.addAlly(new Honchkrow("Bibich", 1));
+        b.addAlly(new Bounsweet("Bubech", 3));
+        b.addFoe(new Tsareena("Jesus", 4));
+        b.addFoe(new Komala("Jake", 3));
         b.go();
     }
 }
